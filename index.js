d077b8e70c81e3385dc57a40ef2f9b099c5d58fa
solb <solbi>
2021-03-05 22:11:43 +0900

    네이버영화평점 크롤링해와서 csv파일에 넣기

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
 const parse = require('csv-parse/lib/sync')
 const fs = require('fs')
 const puppeteer = require('puppeteer')
-
+const stringify = require('csv-stringify/lib/sync')
 const csv = fs.readFileSync('csv/data.csv')
 const records = parse(csv.toString('utf-8'))
 
 const crawler = async () => {
   try {
-    const browser = await puppeteer.launch({ headless: process.env === 'production' })
+    const result = []
+    const browser = await puppeteer.launch({ headless: process.env === 'production' }) // headless : 크롬 안 띄우고 백그라운드에서 실행
     await Promise.all(records.map(async (r, i) => {
       try {
         const page = await browser.newPage()
         await page.goto(r[1])
         const scoreEl = await page.$('.score.score_left .star_score')
         if (scoreEl) {
-          const text = await page.evaluate(tag => tag.textContent,scoreEl)
-          console.log(r[0], '평점', text.trim())
+          const text = await page.evaluate(tag => tag.textContent, scoreEl)
+          result[i] = [r[0], r[1], text.trim()]
         }
-        await page.waitFor(3000)
+        await page.waitFor(10000)
         await page.close()
       } catch (e) {
         console.error(e)
       }
     }))
     await browser.close()
+    const str = stringify(result)
+    fs.writeFileSync('csv/result.csv', str)
   } catch (e) {
     console.error(e)
   }
-
-  // await page.waitFor(3000)
-  // await page.goto('https://google.com')
-  // await page.waitFor(1000)
-  // await page.close()
-  // await browser.close()
 }
 
 crawler()
