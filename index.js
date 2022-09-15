1f2477f48bf5325c44dc60a50a6c1bf23751f356
SolbiShin <sbshin.dev@gmail.com>
2022-09-16 00:11:21 +0900

    unsplash 이미지 파일 다운로드 크롤러 완성

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 const puppeteer = require("puppeteer");
+const axios = require("axios");
 const fs = require("fs");
-const { default: axios } = require("axios");
+
+// 폴더 만들기
+fs.readdir("imgs", (err) => {
+  if (err) {
+    console.error("imgs 폴더가 없어서 imgs폴더를 생성합니다.");
+    fs.mkdirSync("imgs");
+  }
+});
 
 const crawler = async () => {
   try {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto("https://unsplash.com");
-    const result = await page.evaluate(() => {});
+    let result = [];
+    while (result.length <= 30) {
+      const imgArray = await page.evaluate(() => {
+        window.scrollTo(0, 0);
+        let imgs = [];
+        const imgEls = document.querySelectorAll("figure");
+        console.log("img", imgEls);
+        if (imgEls.length) {
+          imgEls.forEach((v) => {
+            let img = v.querySelector("img.YVj9w"); // 사이트 바뀌었을 때 클래스 적절히 바꾸기
+            if (img && img.src) {
+              imgs.push(img.src);
+            }
+            v.parentElement.removeChild(v);
+          });
+        }
+        window.scrollBy(0, 100);
+        setTimeout(() => {
+          window.scrollBy(0, 200);
+        }, 500);
+        return imgs;
+      });
+      result = result.concat(imgArray);
+      await page.waitForSelector("figure");
+      console.log("새 이미지 태그 로딩 완료!");
+    }
+    console.log("result", result);
+    result.forEach(async (src) => {
+      const imgResult = await axios.get(src.replace(/\?.*$/, ""), {
+        responseType: "arraybuffer",
+      });
+      fs.writeFileSync(`imgs/${new Date().valueOf()}.jpeg`, imgResult.data);
+    });
+    await page.close();
+    await browser.close();
   } catch (e) {
     console.error(e);
   }
